d1c7f6125c7b9184b6d1239ab2e80f11dee97f90
jonboh <jon.bosque.hernando@gmail.com>
2025-10-31 12:09:03 +0100

    use omyrepl if available

--- extra_configs/julia_config/startup.jl
+++ extra_configs/julia_config/startup.jl
@@ -2,3 +2,12 @@ using Pkg
 if isfile("Project.toml") && isfile("Manifest.toml")
     Pkg.activate(".")
 end
+
+atreplinit() do repl
+    try
+        @eval using OhMyREPL
+        @eval enable_autocomplete_brackets(false)
+    catch e
+        @warn "error while importing OhMyREPL" e
+    end
+end
