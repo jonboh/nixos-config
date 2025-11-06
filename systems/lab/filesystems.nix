54046e53132429b095564f966f6c4f5b600b6a5b
jonboh <jon.bosque.hernando@gmail.com>
2025-11-06 17:42:43 +0100

    move to LVM disks

--- systems/lab/filesystems.nix
+++ systems/lab/filesystems.nix
@@ -1,20 +1,11 @@
 {sensitive, ...}: {
   fileSystems."/" = {
-    device = "/dev/mapper/crypted";
+    device = "/dev/vg0/root";
     fsType = "ext4";
   };
 
   # Needed to find the USB device during initrd stage
   boot.initrd.kernelModules = ["usb_storage"];
-
-  boot.initrd.luks.devices."crypted" = {
-    device = "/dev/disk/by-partlabel/nixospart";
-    allowDiscards = true;
-    keyFileSize = 4096;
-    fallbackToPassword = true;
-    # pinning to /dev/disk/by-id/usbkey works
-    keyFile = "/dev/disk/by-id/${sensitive.hardware.id.lab-key}";
-  };
 
   fileSystems."/boot" = {
     device = "/dev/disk/by-label/boot";
