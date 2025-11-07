77f577cb90e61a829ff0d8eccb0fc58315617be3
jonboh <jon.bosque.hernando@gmail.com>
2025-11-07 15:22:53 +0100

    make ssh store reachable

--- systems/lab/builders.nix
+++ systems/lab/builders.nix
@@ -1,14 +1,14 @@
-{
+{sensitive, ...}: {
   nix.buildMachines = [
     # NOTE: you can force the usage of the local machine by running:
     # nixos-rebuild <args> --builders ""
-    # {
-    #   hostName = "localhost";
-    #   protocol = null;
-    #   system = "x86_64-linux";
-    #   supportedFeatures = ["kvm" "nixos-test" "big-parallel" "benchmark"];
-    #   maxJobs = 8;
-    # }
+    {
+      hostName = "localhost";
+      protocol = null;
+      system = "x86_64-linux";
+      supportedFeatures = ["kvm" "nixos-test" "big-parallel" "benchmark"];
+      maxJobs = 2;
+    }
     {
       hostName = "tars.lan";
       system = "aarch64-linux";
@@ -33,4 +33,22 @@
     }
   ];
   nix.distributedBuilds = true;
+  nix = {
+    settings = {
+      # min-free = 32 * 1024 * 1024;
+      # max-free = 64 * 1024 * 1024;
+
+      max-jobs = 2;
+      cores = 1;
+    };
+  };
+
+  users.users.nixremote = {
+    isNormalUser = true;
+    createHome = false;
+    openssh.authorizedKeys.keys = [
+      sensitive.keys.ssh.root-workstation
+    ];
+  };
+  users.groups.nixremote = {};
 }
