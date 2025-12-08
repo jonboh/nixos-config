996651879562fa8a0801dabdaedadf0b722ea772
jonboh <jon.bosque.hernando@gmail.com>
2025-12-08 13:23:51 +0100

    update kex algos due to post-quantum warning

--- systems/raspberrys/common/configuration.nix
+++ systems/raspberrys/common/configuration.nix
@@ -53,27 +53,16 @@ in {
         ClientAliveCountMax = 3;
 
         KexAlgorithms = [
+          "sntrup761x25519-sha512@openssh.com"
+          "curve25519-sha256"
           "curve25519-sha256@libssh.org"
-          "ecdh-sha2-nistp521"
-          "ecdh-sha2-nistp384"
           "ecdh-sha2-nistp256"
+          "ecdh-sha2-nistp384"
+          "ecdh-sha2-nistp521"
           "diffie-hellman-group-exchange-sha256"
-        ];
-        Ciphers = [
-          "chacha20-poly1305@openssh.com"
-          "aes256-gcm@openssh.com"
-          "aes128-gcm@openssh.com"
-          "aes256-ctr"
-          "aes192-ctr"
-          "aes128-ctr"
-        ];
-        Macs = [
-          "hmac-sha2-512-etm@openssh.com"
-          "hmac-sha2-256-etm@openssh.com"
-          "umac-128-etm@openssh.com"
-          "hmac-sha2-512"
-          "hmac-sha2-256"
-          "umac-128@openssh.com"
+          "diffie-hellman-group16-sha512"
+          "diffie-hellman-group18-sha512"
+          "diffie-hellman-group14-sha256"
         ];
       };
     };
