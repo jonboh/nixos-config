d10da3112af252f6f6eb9a42b1b1ad6ad148946b
jonboh <jon.bosque.hernando@gmail.com>
2025-12-12 10:43:31 +0100

    add revise julia

--- extra_configs/julia_config/startup.jl
+++ extra_configs/julia_config/startup.jl
@@ -11,3 +11,9 @@ atreplinit() do repl
         @warn "error while importing OhMyREPL" e
     end
 end
+
+try
+    using Revise
+catch e
+    @warn "Error initializing Revise" exception = (e, catch_backtrace())
+end
