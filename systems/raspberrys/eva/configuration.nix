3fca16af0463d2c7b9d07c0aa60df7b4c9f801ce
jonboh <jon.bosque.hernando@gmail.com>
2025-12-28 13:15:21 +0100

    cleanup eva

--- systems/raspberrys/eva/configuration.nix
+++ systems/raspberrys/eva/configuration.nix
@@ -1,19 +1,9 @@
-{
-  self,
-  pkgs,
-  sensitive,
-  ...
-}: {
+{sensitive, ...}: {
   imports = [
     ../../common/raspberrys.nix
     ./sops.nix
-    ./rp-configtxt.nix
+    # ./rp-configtxt.nix
   ];
-  # bcm2711 for rpi 3, 3+, 4, zero 2 w
-  # bcm2712 for rpi 5
-  # See the docs at:
-  # https://www.raspberrypi.com/documentation/computers/linux_kernel.html#native-build-configuration
-  raspberry-pi-nix.board = "bcm2712";
 
   networking = {
     hostName = "eva";
@@ -37,13 +27,6 @@
       ${sensitive.network.ip.tars.lab} tars.lan
     ''; # actually needed to make samba work without timeouts due to missing DNS/Gateway
   };
-
-  environment.systemPackages = with pkgs; [
-    emacs
-    git
-    wget
-    tcpdump
-  ];
 
   system.stateVersion = "24.11";
 }
