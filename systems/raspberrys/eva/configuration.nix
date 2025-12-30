c4122ce7d4fd70444f1cb80f3380f72cec19a3ca
jonboh <jon.bosque.hernando@gmail.com>
2025-12-30 09:44:36 +0100

    configure eva's swap

--- systems/raspberrys/eva/configuration.nix
+++ systems/raspberrys/eva/configuration.nix
@@ -28,5 +28,27 @@
     ''; # actually needed to make samba work without timeouts due to missing DNS/Gateway
   };
 
+  zramSwap = {
+    enable = true;
+    priority = 20;
+  };
+  swapDevices = [
+    {
+      size = 16 * 1024;
+      priority = 10;
+      device = "/var/lib/swapfile";
+      randomEncryption.enable = true;
+    }
+  ];
+  boot.kernel.sysctl = {
+    "vm.swappiness" = 10;
+  };
+  boot.kernelParams = [
+    "zswap.enabled=1" # enables zswap
+    "zswap.compressor=lz4" # compression algorithm
+    "zswap.max_pool_percent=20" # maximum percentage of RAM that zswap is allowed to use
+    "zswap.shrinker_enabled=1" # whether to shrink the pool proactively on high memory pressure
+  ];
+
   system.stateVersion = "24.11";
 }
