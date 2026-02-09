a3247f49910eba4a3a23ee126d1b7a8036f1ccc3
jonboh <jon.bosque.hernando@gmail.com>
2026-02-09 17:07:27 +0100

    fix screenshot xclip integration

--- scripts/screenshot_full.nix
+++ scripts/screenshot_full.nix
@@ -1,5 +1,5 @@
 {pkgs ? import <nixpkgs> {}}:
 pkgs.writeShellScriptBin "screenshot" ''
   FILE=$(date +$HOME/screenshots/%Y-%m-%d-%H_%M_%S.png)
-  mkdir -p $HOME/screenshots && scrot $FILE -e 'xclip -selection clipboard -t image/png -i $f' && krita --nosplash --canvasonly --workspace Vault $FILE
+  mkdir -p $HOME/screenshots && scrot $FILE && xclip -selection clipboard -t image/png -i $FILE && krita --nosplash --canvasonly --workspace Vault $FILE
 ''
