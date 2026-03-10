956c1eee697ada6fb3b97b68209faec8d754f7c5
jonboh <jon.bosque.hernando@gmail.com>
2026-03-10 20:57:43 +0100

    add smoke alarm configuration

--- systems/raspberrys/bragi/home-assistant.nix
+++ systems/raspberrys/bragi/home-assistant.nix
@@ -3,7 +3,144 @@
   lib,
   sensitive,
   ...
-}: {
+}: let
+  smokeAlarmConfig = {
+    notificationData = {
+      sticky = true;
+      persistent = true;
+      priority = "max";
+      importance = "max";
+      ttl = 0;
+      vibrationPattern = "100, 1000, 100, 1000, 100, 1000, 100, 1000, 100, 1000, 100, 1000, 100";
+      visibility = "public";
+    };
+    repeatConditions = [
+      {
+        condition = "state";
+        entity_id = "input_boolean.smoke_alarm_acknowledged";
+        state = "off";
+      }
+    ];
+    repeatTriggers = [
+      {
+        platform = "time_pattern";
+        seconds = "/15";
+      }
+    ];
+    clearAcknowledgement = {
+      service = "input_boolean.turn_off";
+      target = {
+        entity_id = "input_boolean.smoke_alarm_acknowledged";
+      };
+    };
+  };
+
+  mkSmokeNotificationAction = {
+    title,
+    message,
+    channel ? "SmokeAlarm",
+    extraData ? {},
+  }: {
+    service = "notify.mobile_app_pixel_8";
+    data = {
+      inherit message title;
+      data = smokeAlarmConfig.notificationData // {inherit channel;} // extraData;
+    };
+  };
+
+  mkRepeatingAlarmAutomation = {
+    alias,
+    description,
+    triggerEntity,
+    triggerCondition,
+    actions,
+  }: {
+    inherit alias description;
+    mode = "restart";
+    trigger =
+      [
+        {
+          platform = "state";
+          entity_id = triggerEntity;
+          to = "on";
+        }
+      ]
+      ++ smokeAlarmConfig.repeatTriggers;
+    condition = [triggerCondition] ++ smokeAlarmConfig.repeatConditions;
+    action = actions;
+  };
+
+  mkClearAcknowledgementAutomation = {
+    alias,
+    description,
+    triggerEntity,
+  }: {
+    inherit alias description;
+    mode = "single";
+    trigger = [
+      {
+        platform = "state";
+        entity_id = triggerEntity;
+        to = "on";
+      }
+    ];
+    condition = [];
+    action = [smokeAlarmConfig.clearAcknowledgement];
+  };
+
+  mkStandardSmokeNotifications = {
+    place,
+    debugPrefix ? "",
+  }: [
+    # TTS Notification
+    (mkSmokeNotificationAction {
+      title = "${debugPrefix}TTS: SMOKE on ${place}!!";
+      message = "TTS";
+      extraData = {
+        media_stream = "alarm_stream_max";
+        tts_text = "Fire on ${place}";
+      };
+    })
+    # Regular Notification
+    (mkSmokeNotificationAction {
+      title = "${debugPrefix}SMOKE on ${place}!!";
+      message = "Smoke on ${place}!!";
+      channel =
+        if debugPrefix != ""
+        then "SmokeAlarm_${place}_Debug"
+        else "SmokeAlarm_${place}";
+    })
+  ];
+
+  # Function to create a complete smoke alarm system (repeating + clear acknowledgement)
+  mkSmokeAlarmSystem = {
+    place,
+    triggerEntity,
+    debugPrefix ? "",
+  }: let
+    systemName =
+      if debugPrefix != ""
+      then "Debug Notification for ${place}"
+      else "Smoke Alert for ${place}";
+  in [
+    (mkRepeatingAlarmAutomation {
+      alias = "${systemName} - Repeating";
+      description = "Send ${systemName} alerts every 15 seconds until acknowledged";
+      inherit triggerEntity;
+      triggerCondition = {
+        condition = "state";
+        entity_id = triggerEntity;
+        state = "on";
+      };
+      actions = mkStandardSmokeNotifications {inherit place debugPrefix;};
+    })
+    (mkClearAcknowledgementAutomation {
+      alias = "Clear Smoke Alarm Acknowledgement for ${systemName}";
+      description = "Clear acknowledgement when ${systemName} is activated";
+      inherit triggerEntity;
+    })
+  ];
+in {
   services = {
     home-assistant = {
       enable = true;
@@ -14,6 +151,77 @@
           trusted_proxies = ["127.0.0.1"];
         };
         mobile_app = {};
+
+        # Input boolean entities
+        input_boolean = {
+          smoke_alarm_acknowledged = {
+            name = "Smoke Alarm Acknowledged";
+            initial = false;
+          };
+          remove_smokealarm_channel = {
+            name = "Remove Smoke Alarm Channel";
+            initial = false;
+          };
+          debug_lab_notification_button = {
+            name = "Debug Lab Notification Button";
+            initial = false;
+          };
+          debug_kitchen_notification_button = {
+            name = "Debug Kitchen Notification Button";
+            initial = false;
+          };
+        };
+
+        automation =
+          # Real smoke detector system
+          (mkSmokeAlarmSystem {
+            place = "lab";
+            triggerEntity = "binary_sensor.smoke_lab_smoke";
+          })
+          ++ (mkSmokeAlarmSystem {
+            place = "kitchen";
+            triggerEntity = "binary_sensor.smoke_kitchen_smoke";
+          })
+          ++
+          # Manual trigger
+          (mkSmokeAlarmSystem {
+            place = "kitchen";
+            triggerEntity = "input_boolean.debug_kitchen_notification_button";
+            debugPrefix = "Debug: ";
+          })
+          ++ (mkSmokeAlarmSystem {
+            place = "lab";
+            triggerEntity = "input_boolean.debug_lab_notification_button";
+            debugPrefix = "Debug: ";
+          })
+          ++
+          # Additional utility automations
+          [
+            {
+              alias = "Clear SmokeAlarm Channel";
+              mode = "single";
+              trigger = [
+                {
+                  platform = "state";
+                  entity_id = "input_boolean.remove_smokealarm_channel";
+                  from = "off";
+                  to = "on";
+                }
+              ];
+              condition = [];
+              action = [
+                {
+                  service = "notify.mobile_app_pixel_8";
+                  data = {
+                    message = "remove_channel";
+                    data = {
+                      channel = "SmokeAlarm"; # Name of the channel you wish to remove
+                    };
+                  };
+                }
+              ];
+            }
+          ];
       };
       configWritable = false; # TODO: just to play around
 
